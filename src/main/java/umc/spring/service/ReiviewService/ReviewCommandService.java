8f75e81a5c7e12a00baa3a77e7826bb57b9b6395
haeun1107 <haeun9394@gmail.com>
2024-06-21 14:37:14 +0900

    feat: 가게에 리뷰 추가하기 API 구현

--- src/main/java/umc/spring/service/ReiviewService/ReviewCommandService.java
+++ src/main/java/umc/spring/service/ReiviewService/ReviewCommandService.java
@@ -1,4 +1,8 @@
 package umc.spring.service.ReiviewService;
 
+import umc.spring.web.dto.ReviewRequestDTO;
+import umc.spring.web.dto.ReviewResponseDTO;
+
 public interface ReviewCommandService {
+    public ReviewResponseDTO addReview(ReviewRequestDTO reviewRequestDTO);
 }
